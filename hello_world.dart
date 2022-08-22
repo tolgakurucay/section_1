b723cdb2b50e64580c98550a14564f4248f46ca7
Tolga Kuruçay <tolgakurucay1446@gmail.com>
2022-08-22 04:23:48 +0300

    ilk commit

---
+++ hello_world.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  String myName = "Tolga";
+  String mySurname = "Kuruçay";
+  int myAge = 22;
+  int characterOfMyNumber = myName.length + mySurname.length;
+  print("My name is " +
+      myName +
+      " " +
+      mySurname +
+      ", my age is " +
+      myAge.toString() +
+      " and character size of my name are " +
+      characterOfMyNumber.toString());
+}
